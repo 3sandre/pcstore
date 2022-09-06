f7daf71e2c1c3a80303405b8aed1feeef1eb5fa8
3sandre <sandrinetohe5@gmail.com>
2022-09-06 14:43:50 +0000

    add popup section

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ if(hamburger){
         navList.classList.toggle("open");
       });
 }
+
+// ==========popup==============
+
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
+
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide-popup");
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide-popup");
+    }, 1000);
+  });
+}
